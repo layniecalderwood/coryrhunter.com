bc9fd7f6109143be7b2ac6f3e053552489c40bba
Laynie <alaynacalderwood1@gmail.com>
2020-09-03 17:50:18 -0600

    img

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ const postCSSPlugins = [
 class RunAfterCompile {
   apply(compiler) {
     compiler.hooks.done.tap('Copy images', function() {
-      fse.copySync('./app/assets/images', './docs/assets/images')
+      fse.copySync('./app/assets/img', './docs/assets/img')
     })
   }
 }
